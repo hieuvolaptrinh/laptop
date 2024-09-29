6ff094861d573438942b4d0e6804a8f351b3626a
hieuvolaptrinh <vndhieuak@gmail.com>
2024-09-29 21:21:19 +0700

    thêm cho vui

--- spring1/src/main/java/start/spring/io/spring1/helloController.java
+++ spring1/src/main/java/start/spring/io/spring1/helloController.java
@@ -1,12 +1,18 @@
-package start.spring.io.spring;
+package start.spring.io.spring1;
+
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class helloController  {
+public class helloController {
     @GetMapping("/")
     public String index() {
         return "Hello, Spring oke! hiếu võ";
     }
+
+    public static void main(String[] args) {
+        System.out.println("Hello, Spring oke! hiếu võ");
+    }
+    // C:\Program Files\Java\jdk-22
 }
