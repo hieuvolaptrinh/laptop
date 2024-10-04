72f6bc25146cef81a7acff734d065ad976da2a9e
hieuvolaptrinh <vndhieuak@gmail.com>
2024-10-05 00:42:41 +0700

    config jsp quá mệt mỏi

--- spring/src/main/java/start/spring/io/spring1/controller/HelloController.java
+++ spring/src/main/java/start/spring/io/spring1/controller/HelloController.java
@@ -6,12 +6,21 @@ import start.spring.io.spring1.service.UserService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloController {
+    // DI: dependency injection
+    private UserService userService;
+
+    public HelloController(UserService userService) {
+        this.userService = userService;
+    }
+
     @RequestMapping("/")
     public String hello() {
-        return "hello"; // trả về hello.jsp
+        String test = this.userService.handelHello();
+        return "hello";
     }
 }
 // @RestController
