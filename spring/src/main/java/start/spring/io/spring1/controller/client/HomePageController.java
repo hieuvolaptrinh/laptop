c481f5979fdbbbe9e58a9fdaa464c140f043284b
hieuvolaptrinh <vndhieuak@gmail.com>
2024-10-30 16:13:38 +0700

    30/10/2024

--- spring/src/main/java/start/spring/io/spring1/controller/client/HomePageController.java
+++ spring/src/main/java/start/spring/io/spring1/controller/client/HomePageController.java
@@ -4,13 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
 @Controller
 public class HomePageController {
-    
-    @GetMapping("/homePage")
-    public String getMethodName(@RequestParam String param) {
-        return new String();
+
+    @GetMapping("/")
+    public String getMethodName() {
+        return "client/homepage/show";
     }
-    
+
 }
